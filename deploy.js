3ecb3b9abdfa8bc65b395444201836c8356cec76
GriffinCodes <griffin@projecteden.gg>
2025-01-12 14:07:44 -0600

    Improve deploy script

--- deploy.js
+++ deploy.js
@@ -1,53 +1,50 @@
 const chokidar = require('chokidar');
 const fs = require('fs-extra');
 const path = require('path');
+const simpleGit = require('simple-git');
 
-// Function to copy file to target directory
+const PADDING = 80
+
+const repo = simpleGit('.');
+async function isIgnored(sourcePath) {
+	return (await repo.checkIgnore([sourcePath])).length !== 0;
+}
+
 async function copyFileOnChange(sourcePath, targetDirectory) {
-	if (!/.(ts|html|scss)$/.test(sourcePath))
+	if (sourcePath.includes('.git') || targetDirectory.includes('.git'))
 		return;
 
 	try {
-		// Full target path
-		const targetPath = path.join(targetDirectory, sourcePath);
-
-		// Copy the file
-		await fs.copy(sourcePath, targetPath);
-		console.log(`${new Date().toISOString()} File modified: ${path.basename(sourcePath)}. Copied to ${targetDirectory}`);
+		let targetPath = path.join(targetDirectory, sourcePath);
+		if (await isIgnored(sourcePath)) {
+			console.error(`${new Date().toISOString()}  ${sourcePath.padEnd(PADDING)} ->   ignored`);
+		} else {
+			await fs.copy(sourcePath, targetPath);
+			console.log(`${new Date().toISOString()}  ${sourcePath.padEnd(PADDING)} ->   ${targetPath}`);
+		}
 	} catch (error) {
 		console.error(`Error copying file: ${error.message}`);
 	}
 }
 
-// Watch directory for changes
-function watchDirectory(sourceDir, targetDir) {
-	console.log(`Watching for changes in: ${sourceDir}`);
+function watchDirectory(targetDir) {
+	console.log(`Watching for changes in: ${__dirname}`);
 
-	// Initialize chokidar for watching the directory
-	const watcher = chokidar.watch(sourceDir, {
+	const watcher = chokidar.watch('.', {
 		persistent: true,
-		ignoreInitial: true // Ignore events for files existing at start
+		ignoreInitial: true
 	});
 
-	// Listen to the 'change' event
-	watcher.on('change', (filePath) => {
-		copyFileOnChange(filePath, targetDir);
-	});
-
-	// Error handling
-	watcher.on('error', (error) => {
-		console.error(`Watcher error: ${error.message}`);
-	});
+	watcher.on('change', (filePath) => copyFileOnChange(filePath, targetDir));
+	watcher.on('error', (error) => console.error(`Watcher error: ${error.message}`));
 }
 
-// Directories to watch and copy
-const sourceDirectory = process.argv[2]; // First argument
-const targetDirectory = process.argv[3]; // Second argument
+const targetDirectory = process.argv[2];
 
-if (!sourceDirectory || !targetDirectory) {
-	console.log("Usage: node script.js <sourceDirectory> <targetDirectory>");
+if (!targetDirectory) {
+	console.log("Usage: node deploy.js <targetDirectory>");
 	process.exit(1);
 }
 
 // Start watching the source directory
-watchDirectory(sourceDirectory, targetDirectory);
+watchDirectory(targetDirectory);
