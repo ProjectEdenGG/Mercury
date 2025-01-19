aaa387ca5a62b1ea774177ec12e2324cf11aa3cd
GriffinCodes <griffin@projecteden.gg>
2025-01-18 18:49:22 -0600

    Display whether youve voted

--- frontend/src/app/pages/vote/vote.component.ts
+++ frontend/src/app/pages/vote/vote.component.ts
@@ -13,25 +13,39 @@ export class VoteComponent {
 	loading: boolean = true
 
 	headers = { name: { name: 'Name' }, count: { name: 'Votes', classes: 'text-end' } };
+	intervalIds: any[] = [];
 
 	constructor(
 		public utils: Utils,
 		public apiService: ApiService,
 	) {
-		this.apiService.getVoteData().subscribe({
-			next: result => {
-				this.voteData = result;
-				console.log('voteData', this.voteData)
-				this.loading = false;
-			},
-			error: ex => {
-				console.error(ex)
-				this.loading = false;
-			}
-		})
+		let update = () => {
+			this.apiService.getVoteData().subscribe({
+				next: result => {
+					this.voteData = result;
+					console.log('voteData', this.voteData)
+					this.loading = false;
+				},
+				error: ex => {
+					console.error(ex)
+					this.loading = false;
+				}
+			})
+		}
+
+		update();
+		this.intervalIds.push(setInterval(() => update(), 5000))
+	}
+
+	ngOnDestroy() {
+		this.intervalIds.forEach(id => clearInterval(id))
 	}
 
 	replaceUsername(value: any) {
 		return value.replaceAll("{{USERNAME}}", this.utils.nerd.username)
+	}
+
+	hasVoted(site: any) {
+		return this.voteData.activeVotes[this.utils.nerd?.uuid]?.includes(site.id);
 	}
 }
