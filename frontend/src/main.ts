723e15a480d363960692f6feeb768d09349860d8
GriffinCodes <griffin@projecteden.gg>
2025-04-07 19:43:38 -0500

    Automatically determine href by environment

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,14 @@
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+let baseHref = environment.baseHref || '/';
+if (environment.baseHref !== '/') {
+	const baseHrefTag = document.createElement('base');
+	baseHrefTag.setAttribute('href', baseHref);
+	document.getElementsByTagName('head')[0].appendChild(baseHrefTag);
+}
 
 document.addEventListener('DOMContentLoaded', async () => {
 	platformBrowserDynamic().bootstrapModule(AppModule).catch(e => console.error(e))
